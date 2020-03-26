545bcb7795c22a033811aa5a8c615ea994f56f8e
Dhiego Hendrix Atencio <dhiego.atencio@gmail.com>
2020-03-25 21:51:10 -0300

    config export

--- Player.gd
+++ Player.gd
@@ -4,27 +4,51 @@ signal hit
 export var speed = 400 # how fast the player will move (pixel/sec)
 
 var screen_size # size of the game window
+var target = Vector2() # add this variable to hold the clicked position
 
 func _ready():
+	hide()
 	screen_size = get_viewport_rect().size
-	# hide()
-	
+
+func start(pos):
+	position = pos
+	target = pos # initial target is the start position
+	show()
+	$CollisionShape2D.disabled = false
+
+func _input(event): # change the target whenever a touch event happens
+	if event is InputEventScreenTouch and event.is_pressed():
+		target = event.position
+
 func _process(delta):
 	var velocity = Vector2() # the player moviment vector
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+
+	# move towards the target and stop when close
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+
+	# if Input.is_action_pressed("ui_right"):
+	# 	velocity.x += 1
+	# if Input.is_action_pressed("ui_left"):
+	# 	velocity.x -= 1
+	# if Input.is_action_pressed("ui_down"):
+	# 	velocity.y += 1
+	# if Input.is_action_pressed("ui_up"):
+	# 	velocity.y -= 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+
 	position += velocity * delta
+	# We still need to clamp the player's position here because on devices that don't
+	# match your game's aspect ratio, Godot will try to maintain it as much as possible
+	# by creating black borders, if necessary.
+	# Without clamp(), the player would be able to move under those borders.
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
@@ -44,7 +68,3 @@ func _on_Player_body_entered(body):
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
